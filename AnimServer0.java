e5be0c826e3648454f3d325eceec1b715f9f506c
TomohiroSakaguchi <stomohiro59@gmail.com>
2018-10-01 16:00:14 +0900

    映像の送出の部分を変更しました

--- AnimServer0.java
+++ AnimServer0.java
@@ -5,7 +5,7 @@ public class AnimServer0{
   public static void main(String[] args) {
     try {
       byte buf[] = new byte[160*120];
-      int i;
+      int i,pixel;
       BufferedInputStream biStream;
       int port = 8000;
       //サーバソケットの作成
@@ -16,11 +16,28 @@ public class AnimServer0{
         Socket s = ss.accept();
         biStream=new BufferedInputStream(new FileInputStream("bane.raw"));
         DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-
+        /*
         for (i = 0; i<=200; i++) {
           biStream.read(buf,0,160*120);
           dos.write(buf,0,160*120);
+          //System.out.println(biStream);
         }
+        s.close();
+        */
+        for (int y = 0; y<120; y++) {
+          for (int x = 0; x<160; x++) {
+            pixel = (int)buf[y*160+x]*2;
+            //System.out.println(pixel);
+            if (pixel < 0) {
+              continue;
+            }
+            else{
+              biStream.read(buf,0,160*120);
+              dos.write(buf,0,160*120);
+            }
+          }
+        }
+        System.out.println("Done.");
         s.close();
       }
     } catch(Exception e) {
